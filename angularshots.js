8e8ebda8595a089839eb1f175b27cbd561ba03e7
luthan <luthan@gmail.com>
2015-08-19 13:54:11 -0500

    added browser feature detect to get ie8

--- angularshots.js
+++ angularshots.js
@@ -1,4 +1,25 @@
 if (Meteor.isClient) {
+  
+Meteor.startup(function(){
+        var ie = (function(){
+          var undef,
+          v = 3,
+          div = document.createElement('div'),
+          all = div.getElementsByTagName('i');
+          while (
+            div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+            all[0]
+          );
+
+          return v > 4 ? v : undef;
+        }());
+        
+        if(ie < 9){
+          alert("This browser is not supported.  Please use Chrome or Firefox");
+        }
+});
+
+
   var app = angular.module("flushotsapp", ['angular-meteor']);
   
   angular.module("flushotsapp").filter('hourDisplay', function(){
@@ -41,6 +62,8 @@ if (Meteor.isClient) {
   angular.module("flushotsapp").controller("FlushotsController", ['$scope','$meteor','$http', function($scope, $meteor, $http){
     $scope.hours = $meteor.collection(Hours);
     $scope.slots = $meteor.collection(Slots);
+    
+
     // $scope.employee = docCookies.getItem("SCRAPEMAIL");
     // console.log($scope.employee);
     // if(!$scope.employee){
@@ -131,6 +154,10 @@ if (Meteor.isServer) {
 // });
   
   Meteor.startup(function () {
+    
+
+  
+  
     // code to run on server at startup
     
     if(Hours.find().count() === 0){
